fd48c172695f87101a386a5db8a6ac0486f7c757
s4kr4 <s4kr4m4@gmail.com>
2018-03-01 22:40:39 +0900

    Update detail page

--- src/pages/activity-detail/activity-detail.module.ts
+++ src/pages/activity-detail/activity-detail.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ActivityDetailPage } from './activity-detail';
 
+import { PipesModule } from '../../pipes/pipes.module';
+
 @NgModule({
   declarations: [
     ActivityDetailPage,
   ],
   imports: [
     IonicPageModule.forChild(ActivityDetailPage),
+    PipesModule,
   ],
 })
 export class ActivityDetailPageModule {}
