6d07aa76f1d0f6f743fcd68076ee55cca383ed71
s4kr4 <s4kr4m4@gmail.com>
2018-03-01 23:02:19 +0900

    Refactor

--- src/pages/activity/activity.module.ts
+++ src/pages/activity/activity.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ActivityPage } from './activity';
 
+import { PipesModule } from '../../pipes/pipes.module';
+
 @NgModule({
   declarations: [
     ActivityPage,
   ],
   imports: [
     IonicPageModule.forChild(ActivityPage),
+    PipesModule,
   ],
 })
 export class ActivityPageModule {}
