0e286997e369e7d596baf3f118580bd5abdf9b7d
s4kr4 <s4kr4m4@gmail.com>
2018-03-05 19:01:04 +0900

    Fix for native

--- src/providers/activity/activity.ts
+++ src/providers/activity/activity.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Platform } from 'ionic-angular';
 
 /*
   Generated class for the ActivityProvider provider.
@@ -11,12 +12,20 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ActivityProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(
+    public http: HttpClient,
+    public platform: Platform,
+  ) {
     console.log('Hello ActivityProvider Provider');
   }
 
   getActivities(username: string): Observable<Object> {
-    return this.http.get(`/github/users/${username}/events`);
+    const url = `users/${username}/events`;
+    if (this.platform.is('mobile')) {
+      return this.http.get(`https://api.github.com/${url}`);
+    } else {
+      return this.http.get(`/github/${url}`);
+    }
   }
 
   getActivity(id: number): Object {
